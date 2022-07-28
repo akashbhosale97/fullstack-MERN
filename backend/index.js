0f80fab5c8614d3329a17c26d0b70ca63148097c
Akash Bhosale <MY_NAME@example.com>
2022-07-29 02:35:29 +0530

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const blog = require("./model/user");
+require("dotenv/config");
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/", async (req, res) => {
+const tank = new blog({
+title: req.body.title,
+body: req.body.body,
+});
+await tank
+.save()
+.then(() => {
+console.log("added successfully");
+res.send("added successfully");
+})
+.catch((err) => console.log(err.message));
+});
+
+mongoose.connect(process.env.MONGODB_URI, () => {
+console.log("connected to mongodb");
+});
+
+app.listen(process.env.PORT || 5000, () => {
+console.log(`Server running on PORT : ${process.env.PORT}`);
+});
---
+++ frontend/src/App.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import axios from "axios";
+function App() {
+const [data, setData] = useState({ title: "", body: "" });
+const handleSubmit = async (e) => {
+e.preventDefault();
+if (data.title !== null && data.body !== null) {
+try {
+await axios.post("http://localhost:5000", {
+title: data.title,
+body: data.body,
+});
+} catch (err) {
+console.log(err.message);
+}
+}
+};
+
+return (
+<div className="wrapper">
+<form onSubmit={handleSubmit}>
+<input
+type="text"
+placeholder="Enter Blog Title"
+onChange={(e) => setData({ ...data, title: e.target.value })}
+/>
+<textarea
+placeholder="Enter Blog Content"
+onChange={(e) => setData({ ...data, body: e.target.value })}
+/>
+<button type="submit">Post</button>
+</form>
+</div>
+);
+}
+
+export default App;
