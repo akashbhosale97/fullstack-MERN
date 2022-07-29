abbc71c28337146fdb0524faff6bd8cbd1797f1b
Akash Bhosale <MY_NAME@example.com>
2022-07-29 12:04:35 +0530

    ...

--- backend/index.js
+++ backend/index.js
@@ -8,23 +8,23 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/", async (req, res) => {
-const tank = new blog({
-title: req.body.title,
-body: req.body.body,
-});
-await tank
-.save()
-.then(() => {
-console.log("added successfully");
-res.send("added successfully");
-})
-.catch((err) => console.log(err.message));
+  const tank = new blog({
+    title: req.body.title,
+    body: req.body.body,
+  });
+  await tank
+    .save()
+    .then(() => {
+      console.log("added successfully");
+      res.send("added successfully");
+    })
+    .catch((err) => console.log(err.message));
 });
 
 mongoose.connect(process.env.MONGODB_URI, () => {
-console.log("connected to mongodb");
+  console.log("connected to mongodb");
 });
 
 app.listen(process.env.PORT || 5000, () => {
-console.log(`Server running on PORT : ${process.env.PORT}`);
+  console.log(`Server running on PORT : ${process.env.PORT}`);
 });
--- backend/model/user.js
+++ backend/model/user.js
@@ -3,15 +3,15 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
 const BlogPost = new Schema({
-author: ObjectId,
-title: {
-type: String,
-required: true,
-},
-body: {
-type: String,
-required: true,
-},
+  author: ObjectId,
+  title: {
+    type: String,
+    required: true,
+  },
+  body: {
+    type: String,
+    required: true,
+  },
 });
 const blog = mongoose.model("blog", BlogPost);
 
